12a28e9f32d6aff5fe154d0d788698248ed92a86
donmahallem <donmahallem@users.noreply.github.com>
2019-10-27 11:08:53 +0100

    aggregated types

--- src/fit-data-types.ts
+++ src/fit-data-types.ts
@@ -13,4 +13,10 @@ export type FitDataTypes = "com.google.activity.sample" | "com.google.activity.s
     "com.google.step_count.delta" |
     "com.google.weight" |
     "com.google.activity.exercise" |
+    "com.google.activity.summary" |
+    "com.google.heart_rate.summary" |
+    "com.google.nutrition.summary" |
+    "com.google.power.summary" |
+    "com.google.speed.summary" |
+    "com.google.weight.summary" |
     string;
