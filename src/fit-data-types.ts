550adc998de47912d74cc36ec4ccfc7331c414f4
donmahallem <donmahallem@users.noreply.github.com>
2019-10-27 11:06:34 +0100

    add datatypes

---
+++ src/fit-data-types.ts
@@ -0,0 +1,16 @@
+export type FitDataTypes = "com.google.activity.sample" | "com.google.activity.segment" |
+    "com.google.calories.consumed" |
+    "com.google.calories.expended" |
+    "com.google.cycling.pedaling.cadence" |
+    "com.google.cycling.wheel_revolution.rpm" |
+    "com.google.distance.delta" |
+    "com.google.heart_rate.bpm" |
+    "com.google.height" |
+    "com.google.nutrition" |
+    "com.google.power.sample" |
+    "com.google.speed" |
+    "com.google.step_count.cadence" |
+    "com.google.step_count.delta" |
+    "com.google.weight" |
+    "com.google.activity.exercise" |
+    string;
