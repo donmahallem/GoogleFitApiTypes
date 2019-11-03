3c1785117be6422885fd9c4b3571049dadf2e248
donmahallem <donmahallem@users.noreply.github.com>
2019-11-03 09:40:00 +0100

    Added missing types to IFitDataset

--- src/fit-dataset.ts
+++ src/fit-dataset.ts
@@ -4,8 +4,16 @@
 
 import { FitDataSourceId } from "./fit-data-source-id";
 import { IFitDatasetPoint } from "./fit-dataset-point";
+import { Long } from "./long";
 
+/**
+ * Dataset used in Fit
+ * https://developers.google.com/fit/rest/v1/reference/users/dataSources/datasets#resource
+ */
 export interface IFitDataset {
-    point: IFitDatasetPoint[];
     dataSourceId: FitDataSourceId;
+    point: IFitDatasetPoint[];
+    maxEndTimeNs: Long;
+    minStartTimeNs: Long;
+    nextPageToken?: string;
 }
